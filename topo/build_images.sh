c60ef7ca5d8b90e652a213390586f8b2eeca3b52
Alan King <alanking@renci.org>
2019-04-09 12:25:01 -0400

    Spacing and topo for me

--- topo/build_images.sh
+++ topo/build_images.sh
@@ -5,12 +5,12 @@ if [ -n "$1" ]; then
     suffix=_$1
 fi
 
-build_args="--build-arg irods_repo=https://github.com/korydraughn/irods"
-build_args="$build_args --build-arg irods_branch=re_plugin_issue_4144_partial"
-build_args="$build_args --build-arg icommands_repo=https://github.com/korydraughn/irods_client_icommands"
-build_args="$build_args --build-arg icommands_branch=4-2-stable"
-build_args="$build_args --build-arg cmake_path=/opt/irods-externals/cmake3.5.2-0/bin"
-build_args="$build_args --build-arg clang_path=/opt/irods-externals/clang3.8-0/bin"
+build_args="--build-arg irods_repo=https://github.com/alanking/irods"
+build_args="$build_args --build-arg irods_branch=master"
+build_args="$build_args --build-arg icommands_repo=https://github.com/alanking/irods_client_icommands"
+build_args="$build_args --build-arg icommands_branch=master"
+build_args="$build_args --build-arg cmake_path=/opt/irods-externals/cmake3.11.4-0/bin"
+build_args="$build_args --build-arg clang_path=/opt/irods-externals/clang6.0-0/bin"
 
 docker build -t irods_topo$suffix:provider -f Dockerfile.provider $build_args .
 docker build -t irods_topo$suffix:consumer -f Dockerfile.consumer $build_args .
