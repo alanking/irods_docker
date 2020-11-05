12f6dec27d51ea96498d01128f582861d0af2dcb
Alan King <alanking@renci.org>
2020-11-05 13:02:55 -0500

    3.3.1 works?

--- 3.3.1/setup_irods.sh
+++ 3.3.1/setup_irods.sh
@@ -2,6 +2,8 @@ git clone https://github.com/irods/irods-legacy
 
 cd irods-legacy/iRODS
 
+# insert #define requireSIDs 1
+
 cat /irods.config
 cp /irods.config config/irods.config
 ./scripts/irodsprompt
